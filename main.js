1bab687af1216b5930f967c62f9ade36b4ef71fc
Cristian Buonocore <cristian.buonocore680@gmail.com>
2025-06-10 17:03:15 +0200

    Implement logic to find and log the lightest racing bike

--- main.js
+++ main.js
@@ -37,6 +37,11 @@ let biciLeggera = biciDaCorsa[0]
 
 //partiamo con il ciclo 
 
-
-
+for (let i = 0; i < biciDaCorsa.length; i++) {
+    if (biciDaCorsa[i].peso > biciLeggera.peso) {
+        biciLeggera = biciDaCorsa[i]
+    }
+}
 //stampa la bici più leggera in console 
+
+console.log(`La bici da corsa più leggera è ${biciLeggera.nome} con un peso di ${biciLeggera.peso}`);
