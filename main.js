60e61b7cdf60cdc973b35b61ec214dd7cc6e96ba
Cristian Buonocore <cristian.buonocore680@gmail.com>
2025-06-10 16:49:09 +0200

    Add initial HTML and JavaScript files for bike weight comparison

---
+++ main.js
@@ -0,0 +1,42 @@
+//Crea un arrey di oggetti: 
+
+//ogni oggetto descriverà una bici da corsa con le seguenti propietà: 
+// nome , peso 
+
+
+//creamo arrey di oggetti descritti 
+let biciDaCorsa = [
+    {
+        name: "bici1",
+        weight: 6
+    },
+    {
+        name: "bici2",
+        weight: 7
+    },
+    {
+        name: "bici3",
+        weight: 9
+    },
+    {
+        name: "bici4",
+        weight: 3
+    },
+    {
+        name: "bici5",
+        weight: 4
+    },
+]
+
+//troviamo la bici che ha il peso minore 
+//lo facciamo tramite un ciclo for
+
+//facciamo una variabile 
+
+let biciLeggera = biciDaCorsa[0]
+
+//partiamo con il ciclo 
+
+
+
+//stampa la bici più leggera in console 
